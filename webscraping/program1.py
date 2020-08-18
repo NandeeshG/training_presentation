398f857083fdc3063bab5b13ec71f38efcd17e15
NandeeshG <nandeeshgupta@gmail.com>
2020-08-18 21:24:28 +0530

    comments added

--- webscraping/program1.py
+++ webscraping/program1.py
@@ -1,6 +1,8 @@
 
 # WEB SCRAPING
-# The program will use urllib to read the HTML from the data files below, and parse the data, extracting numbers and compute the sum of the numbers in the file.
+# The program will use urllib to read the HTML from the data 
+# files below, and parse the data, extracting numbers and 
+# compute the sum of the numbers in the file.
 
 import re
 import urllib.request, urllib.parse, urllib.error
